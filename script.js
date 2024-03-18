e03fa6c88abc4f1735ec6d3f91655228df1d1003
Adebayo <adenguk5251@gmail.com>
2024-03-18 14:00:13 +0000

    addfhgdh

--- script.js
+++ script.js
@@ -1,56 +1,92 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Display current day
-    var currentDate = dayjs().format('dddd, MMMM D, YYYY');
-    document.getElementById('current-day').textContent = currentDate;
+var today = moment().format("dddd, MMMM Do YYYY ");
 
-    // Generate time blocks for standard business hours (9AM to 5PM)
-    var timeBlocks = document.getElementById('time-blocks');
-    for (var i = 9; i <= 17; i++) {
-        var timeBlock = createTimeBlock(i);
-        timeBlocks.appendChild(timeBlock);
-    }
+var now = moment().format("H A");
 
-    // Check and apply past, present, future colors
-    checkTimeBlocks();
+// current day
+
+$("#currentDay").text(today);
+
+
+/* planWorkday entries for each hour of the workday */
+var planWorkday = [
+    { time: "9 AM", 
+        event: "" },
+    { time: "10 AM", 
+        event: "" },
+    { time: "11 AM", 
+        event: "" },
+    { time: "12 PM", 
+        event: "" },
+    { time: "1 PM", 
+        event: "" },
+    { time: "2 PM", 
+        event: "" },
+    { time: "3 PM", 
+        event: "" },
+    { time: "4 PM", 
+        event: "" },
+    { time: "5 PM", 
+        event: "" },
+	{ time: "6 PM", 
+        event: "" },
+  ];
+
+/* Local Storage check */
+var workEvents = JSON.parse(localStorage.getItem("workDay"));
+if (workEvents) {
+  planWorkday = workEvents;
+}
+
+/* Current Day */
+$("#currentDay").text(today);
+
+/* Create rows */
+planWorkday.forEach(function(timeBlock, index) {
+	var timeLabel = timeBlock.time;
+	var blockColor = colorRow(timeLabel);
+	var row =
+		'<div class="time-block" id="' +
+		index +
+		'"><div class="row no-gutters input-group"><div class="col-sm col-lg-1 input-group-prepend hour justify-content-sm-end pr-3 pt-3">' +
+		timeLabel +
+		'</div><textarea class="form-control ' +
+		blockColor +
+		'">' +
+		timeBlock.event +
+		'</textarea><div class="col-sm col-lg-1 input-group-append"><button class="saveBtn btn-block" type="submit"><i class="fas fa-save"></i></button></div></div></div>';
+
+	/* Adding rows to container div */
+	$(".container").append(row);
 });
 
-function createTimeBlock(hour) {
-    var timeBlock = document.createElement('div');
-    timeBlock.classList.add('time-block');
-
-    var timeLabel = document.createElement('div');
-    timeLabel.textContent = dayjs().hour(hour).format('hA');
-    timeBlock.appendChild(timeLabel);
-
-    var eventInput = document.createElement('textarea');
-    timeBlock.appendChild(eventInput);
-
-    var saveButton = document.createElement('button');
-    saveButton.textContent = 'Save';
-    saveButton.addEventListener('click', function() {
-        saveEvent(hour, eventInput.value);
-    });
-    timeBlock.appendChild(saveButton);
-
-    return timeBlock;
+/* Color rows based on current time */
+function colorRow(time) {
+	var planNow = moment(now, "H A");
+	var planEntry = moment(time, "H A");
+	if (planNow.isBefore(planEntry) === true) {
+		return "future";
+	} else if (planNow.isAfter(planEntry) === true) {
+		return "past";
+	} else {
+		return "present";
+	}
 }
 
-function checkTimeBlocks() {
-    var currentTime = dayjs().hour();
+/* Save Events */
+$(".saveBtn").on("click", function() {
+	var blockID = parseInt(
+		$(this)
+			.closest(".time-block")
+			.attr("id")
+	);
+	var userEntry = $.trim(
+		$(this)
+			.parent()
+			.siblings("textarea")
+			.val()
+	);
+	planWorkday[blockID].event = userEntry;
 
-    var timeBlocks = document.getElementsByClassName('time-block');
-    for (var i = 0; i < timeBlocks.length; i++) {
-        var timeBlockHour = parseInt(dayjs(timeBlocks[i].firstChild.textContent, 'hA').format('H'));
-        if (timeBlockHour < currentTime) {
-            timeBlocks[i].classList.add('past');
-        } else if (timeBlockHour === currentTime) {
-            timeBlocks[i].classList.add('present');
-        } else {
-            timeBlocks[i].classList.add('future');
-        }
-    }
-}
-
-function saveEvent(hour, event) {
-    localStorage.setItem('event_' + hour, event);
-}
+	/* Set local storage */
+	localStorage.setItem("workDay", JSON.stringify(planWorkday));
+});
